544eaf2931ebe6d7998610b82d8147652e76d1de
tanvi-vasoya <vasoyatanvi203@gmail.com>
2026-09-13 01:10:03 +0530

    Update database.py
    Added comments to clarify database connection setup and session management.

--- app/database.py
+++ app/database.py
@@ -5,25 +5,24 @@ from sqlalchemy.orm import Session
 from sqlalchemy.orm import sessionmaker
 from app.config import settings
 
-
+#This code sets up a database connection layer using SQLAlchemy 2.0. It is designed to automatically manage opening, using, and closing database connections for every individual web request.
 engine = create_engine(
     settings.database_url,
     echo=settings.debug,
 )
+## CREATE A SESSION OBJECT TO INITIATE QUERY 
 SessionLocal = sessionmaker(
     bind=engine,
-    autoflush=False,
-    autocommit=False,
+    autoflush=False,  #Stops SQLAlchemy from sending data to the database before you explicitly ask it to.
     expire_on_commit=False,
 )
-
+#combines table definition and class mapping into a single, unified syntax using a base class.
 class Base(DeclarativeBase):
     """Base class for all SQLAlchemy ORM models."""
     pass
-    
+#get_db method to create database sessions when needed.
 def get_db() -> Generator[Session, None, None]:
     """Create a new database session for each request."""
-
     db = SessionLocal()
     try:
         yield db
