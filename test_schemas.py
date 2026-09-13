0b388c80e2ce6c5b253e1090329ae0651618b6d6
tanvi-vasoya <vasoyatanvi203@gmail.com>
2026-09-13 18:28:13 +0530

    Delete test_schemas.py

--- test_schemas.py
+++
@@ -1,7 +0,0 @@
-from app.schemas.trip import TripCreate
-from app.schemas.participant import ParticipantCreate
-from app.schemas.survey_response import SurveyResponseCreate
-from app.schemas.recommendation import RecommendationCreate
-from app.schemas.vote import VoteCreate
-
-print("All schemas imported successfully!")
