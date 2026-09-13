ef6403306b0aef728bd958ddcf41f6f86a35a496
tanvi-vasoya <vasoyatanvi203@gmail.com>
2026-09-13 18:29:09 +0530

    Delete test_twilio_config.py

--- test_twilio_config.py
+++
@@ -1,7 +0,0 @@
-from app.config import settings
-
-print("=" * 50)
-print("ENABLE_SMS        :", settings.enable_sms)
-print("ACCOUNT SID       :", settings.twilio_account_sid)
-print("PHONE NUMBER      :", settings.twilio_phone_number)
-print("=" * 50)
